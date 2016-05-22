522dc22d1ef4848cf1d0cd8d8b81dadeb6696f7e
akira <akira@192.168.10.3>
2016-05-22 11:37:28 +0900

    first commit

---
+++ src/main/java/jp/co/yohachi_suga810/helloWorldSample/App.java
@@ -0,0 +1,13 @@
+package jp.co.yohachi_suga810.helloWorldSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
