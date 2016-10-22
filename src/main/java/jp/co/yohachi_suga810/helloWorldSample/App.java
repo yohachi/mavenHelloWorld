f29e0b450831cca90cfc10236a81eddda96bcacd
akira <akira@akira-PC>
2016-10-22 23:14:53 +0900

    rebase commit

--- src/main/java/jp/co/yohachi_suga810/helloWorldSample/App.java
+++ src/main/java/jp/co/yohachi_suga810/helloWorldSample/App.java
@@ -4,10 +4,10 @@ package jp.co.yohachi_suga810.helloWorldSample;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!ab" );
     }
 }
